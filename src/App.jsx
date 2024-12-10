aff7be50ad8dcdf4871a746f72fefd2c0fcf576d
rajan <rajan8871@gmail.com>
2024-12-10 20:07:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import './App.css'
+import Raja from './components/Raja'
+
+function App() {
+
+  return (
+    <>
+    <h3>SAdsf</h3>
+    <h1>dxhgc</h1>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    <Raja/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Raja.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Raja (){
+   return(
+    <>
+    
+    <h1>text </h1>
+    
+    </>
+   )
+}
+
+export default Raja;
